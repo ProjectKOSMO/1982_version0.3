98b356f3a76ae57e56a4f328cc38006b7ece47f6
ProjectKOSMO <project30001@gmail.com>
2021-12-31 18:08:00 +0900

    1231 성훈

--- src/main/java/com/javassem/controller/ShopController.java
+++ src/main/java/com/javassem/controller/ShopController.java
@@ -1,5 +1,7 @@
 package com.javassem.controller;
 
+
+import java.util.HashMap;
 import java.util.List;
 
 import org.mybatis.spring.SqlSessionTemplate;
@@ -10,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.javassem.domain.OwnerBoardVO;
-import com.javassem.domain.OwnerVO;
 import com.javassem.domain.ShopVO;
 import com.javassem.service.OwnerService;
 import com.javassem.service.ShopService;
@@ -32,17 +32,17 @@ public class ShopController {
 	  private SqlSessionTemplate mybatis;	
 	
 	// 일자리 찾기 업체 가져오기.
-//	@RequestMapping(value="/user/storeClose.do", method=RequestMethod.GET)
-//	public String select(ShopVO vo, Model m,String searchCondition, String searchKeyword ){
-//		
-//		HashMap map = new HashMap();
-//		map.put("searchCondition", searchCondition);
-//		map.put("searchKeyword", searchKeyword);
-//		
-//		List<ShopVO> list = shopService.getShopList(map);
-//		m.addAttribute("ShopList", list);
-//		return "user/storeClose" ;
-//	}
+	@RequestMapping(value="storeCloseget.do", method=RequestMethod.GET)
+	public String select(ShopVO vo, Model m,String searchCondition, String searchKeyword ){
+		
+		HashMap map = new HashMap();
+		map.put("searchCondition", searchCondition);
+		map.put("searchKeyword", searchKeyword);
+		
+		List<ShopVO> list = shopService.getShopList(map);
+		m.addAttribute("ShopList", list);
+		return "user/storeClose" ;
+	}
 	
 
 	@RequestMapping(value = {"userSupport.do"}, method=RequestMethod.GET)
@@ -70,7 +70,9 @@ public class ShopController {
 			@RequestParam String jobDate,
 			@RequestParam String startTime,
 			@RequestParam String endTime,
-			ShopVO vo, Model m){
+			@RequestParam String userid,
+			
+			ShopVO vo, Model m) {
 			
 			vo.setBoard_owner_seq(board_owner_seq);
 			vo.setShopname(shopname);
@@ -80,21 +82,31 @@ public class ShopController {
 			vo.setJobDate(jobDate);
 			vo.setJobTime_start(startTime);
 			vo.setJobTime_end(endTime);
+			vo.setUserid(userid);
 			
-			mybatis.insert("ShopDAO.getShop2", vo);
-			return "redirect:storeClose.do";
+			
+			ShopVO y = mybatis.selectOne("ShopDAO.getSupport", vo);
+			
+			if(y != null){
+				return "redirect:storeClose.do";
+							
+			}else{
+				
+				mybatis.insert("ShopDAO.getShop2", vo);
+				return "redirect:storeClose.do";
+			}
 		
 
 	}
 
-  
+
+
 	// 일자리 찾기 버튼 클릭시 동작하는 코드
     @RequestMapping({"storeClose.do"})
     public void select(ShopVO vo, Model m) {
     	
         List<ShopVO> list = shopService.ShopList(vo);
         m.addAttribute("ShopList", list);
-     System.out.println("또여기냐?");
      
         
     }
